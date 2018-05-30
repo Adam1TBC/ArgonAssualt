dfb7b639ba901452227b46c5422ceda5e374909b
Adam <Twilightsparkleadam@gmail.com>
2018-05-31 02:50:07 +0300

    Added splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    [SerializeField] float delay = 3f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadFirstScene", delay);
+	}
+	
+	void LoadFirstScene() {
+        SceneManager.LoadScene(0);
+    }
+}
