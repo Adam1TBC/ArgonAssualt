42f4a9ca6fc346e589b7e5990509388a82982534
Adam <Twilightsparkleadam@gmail.com>
2018-06-04 20:38:04 +0300

    Added circuit waypoint animation

--- Assets/Packs/Standard Assets/Utility/WaypointCircuit.cs
+++ Assets/Packs/Standard Assets/Utility/WaypointCircuit.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fce5c9dfc0c4cfd2928e9580d09231f97831e6b53bb8bfd297110ff8905a607
-size 13276
+oid sha256:42f695436e69d1573acd2a23b9acee18afc41de556a3fced73247d8213ff793d
+size 13275
